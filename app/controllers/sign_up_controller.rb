92ce70722781e210d241219770605b2c73e06d72
John Kahookele <kahookelekelii@gmail.com>
2024-04-25 06:44:38 -1000

    adds page for sign up

--- app/controllers/sign_up_controller.rb
+++ app/controllers/sign_up_controller.rb
@@ -1,8 +1,15 @@
 class SignUpController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
 end
